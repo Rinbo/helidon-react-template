c1a8b94a342641116f7662a3a44159a810a1425a
Robin Börjesson <robin.b@outlook.com>
2024-07-22 15:05:01 +0200

    Add a more appealing hero section

--- src/main/frontend/src/views/landing.tsx
+++ src/main/frontend/src/views/landing.tsx
@@ -1,26 +1,34 @@
 import { Link } from "react-router-dom";
 import LogoutForm from "./logout/logout-form.tsx";
 import { useAuth } from "./main-layout.tsx";
+import { HiTemplate } from "react-icons/hi";
 
 export default function Landing() {
   const { principal } = useAuth();
 
   return (
     <section className="flex h-full items-center justify-center">
-      <div className="hero mx-auto max-w-xl rounded-xl">
-        <div className="hero-content text-center">
+      <div className="hero max-w-lg rounded-3xl py-8 text-center sm:px-8">
+        <div className="hero-content flex-col">
+          <HiTemplate className="text-9xl text-primary" />
           <div>
             <h1 className="my-10 font-mono text-4xl font-bold md:text-5xl">borjessons.dev</h1>
-            <p className="py-6">Hello there and welcome</p>
-            <Link className="link-info" to="/about">
-              About
-            </Link>
-            {principal && <LogoutForm />}
-            {!principal && (
-              <Link className="link-info block" to={"/login"}>
-                Login
+            <p className="py-6">
+              Revolutionize your workflow with <span className="font-mono text-primary">borjessons.dev</span>. Our cutting-edge platform
+              integrates seamlessly with your existing tools, boosting productivity and streamlining collaboration across your entire
+              organization.
+            </p>
+            <div className="flex flex-row justify-center gap-4">
+              <Link className="btn btn-primary" to="/about">
+                About
               </Link>
-            )}
+              {principal && <LogoutForm className="btn btn-secondary" />}
+              {!principal && (
+                <Link className="btn btn-secondary" to={"/login"}>
+                  Login
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
